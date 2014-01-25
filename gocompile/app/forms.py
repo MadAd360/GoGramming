ba1edb432677b7ee4b899e6b31dc8bfe71241b4d
Admin <pi@GoCompile.(none)>
2014-01-25 23:19:13 +0000

    User account creation

--- gocompile/app/forms.py
+++ gocompile/app/forms.py
@@ -1,8 +1,37 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
+from wtforms import TextField, BooleanField, PasswordField
 from wtforms.validators import Required
+from models import User
 
 class LoginForm(Form):
     username = TextField('username', validators = [Required()])
-    password = TextField('password', validators = [Required()])
+    password = PasswordField('password', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
+    
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        # regular validation
+        rv = Form.validate(self)
+        if not rv:
+            return False
+
+        user = User.query.filter_by(nickname=self.username.data).first()
+        if user is None:
+            self.username.errors.append('Unknown username')
+            return False
+
+        if not user.check_password(self.password.data):
+            self.password.errors.append('Invalid password')
+            return False
+
+        self.user = user
+        return True
+
+class CreateForm(Form):
+    username = TextField('username', validators = [Required()])
+    email = TextField('email', validators = [Required()])
+    password = PasswordField('password', validators = [Required()])
+
--- gocompile/app/views.py
+++ gocompile/app/views.py
@@ -1,11 +1,25 @@
-from flask import render_template, flash, redirect
-from app import app
-from forms import LoginForm
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm
+from forms import LoginForm, CreateForm
+from models import User, ROLE_USER, ROLE_ADMIN
+import os
+import crypt
+from git import *
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = { 'nickname': 'Duncan' } # fake user
+    user = g.user
     posts = [ # fake array of posts
         { 
             'author': { 'nickname': 'John' }, 
@@ -23,10 +37,41 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-    	flash('Login requested for OpenID="' + form.username.data + '", remember_me=' + str(form.remember_me.data))
-    	return redirect('/index')
+        #session['remember_me'] = form.remember_me.data
+	login_user(form.user, form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', 
         title = 'Sign In',
         form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/create', methods = ['GET', 'POST'])
+def create():
+    form = CreateForm()
+    if form.validate_on_submit():
+	user = User(nickname=form.username.data, password=form.password.data, email=form.email.data, role=ROLE_USER)
+	db.session.add(user)
+	db.session.commit()
+	newuser = user.nickname
+	password = crypt.crypt(user.password,"22")
+        os.system("sudo useradd -M -p " + password + " " + newuser)
+	os.system("sudo mkdir /srv/local/" + newuser)
+        os.system("sudo mkdir /srv/git/" + newuser)
+	os.system("sudo chmod 777 /srv/git/" + newuser)
+        os.system("sudo chmod 777 /srv/local/" + newuser)
+	repo = Repo.init("/srv/git/" + newuser + "/", bare=True)
+	working_repo = repo.clone("/srv/local/" + newuser + "/")
+	#add user file next to git repo
+        return redirect(url_for('login'))
+    return render_template('createAccount.html',
+        title = 'Create New Account',
+        form = form)
+
