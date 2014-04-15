19da1fe27e2f62de97de5cf7736b3b51436ecb2f
Admin <pi@GoCompile.(none)>
2014-04-15 19:32:45 +0000

    Update to values in configuration files

--- gogramming/config.py
+++ gogramming/config.py
@@ -4,21 +4,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '$2a$16$PnnI'
 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:runner6@localhost/gocompiledb"
+SQLALCHEMY_DATABASE_URI = "postgresql://<username>:<password>@localhost/gocompiledb"
 SQLALCHEMY_MIGRATE_REPO = basedir + "/db_repository"
 
 SECURITY_PASSWORD_HASH = 'sha512_crypt'
 SECURITY_PASSWORD_SALT = '$2a$16$PnnIgfMwkOjGX4SkHqSOPO'
 
 # email server
-MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_SERVER = '<emailserver>'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = 'gogramminguk'
-MAIL_PASSWORD = 'adminpass24'
+MAIL_USERNAME = '<emailusername>'
+MAIL_PASSWORD = '<emailpassword>'
 
 # administrator list
-ADMINS = ['gogramminguk@gmail.com']
+ADMINS = ['<emailaddress>']
 
 
